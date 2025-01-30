287aa3bfefcfcb8cf0fb260f7bbf5fd4ff4d79f5
Timothy Roberts <timber110406@gmail.com>
2025-01-29 20:06:33 -0800

    added calendar

--- app/page.js
+++ app/page.js
@@ -1,16 +1,17 @@
 import Navbar from "@/components/navbar/navbar";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
+import Calendar from "@/components/calendar/calendar";
 
 export default function Home() {
   return (
-   <div className={styles.homepage}>
-   <Navbar />
-   <div className={styles.homepageCard}>
-    
-    <h1 className={styles.h1}>
-      Hi Kylynn! This is going to be your home page
-    </h1>
-   </div>
-   </div>
+    <div className={styles.homepage}>
+      <Navbar />
+      <div className={styles.homepageCard}>
+        <h1 className={styles.h1}>
+          Hi Kylynn! This is going to be your home page
+        </h1>
+        <Calendar />
+      </div>
+    </div>
   );
 }
