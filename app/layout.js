0a0b19b54ae25749e2dd9c035490bf6abfcfb1b2
Timothy Roberts <timber110406@gmail.com>
2025-02-21 12:54:45 -0800

    commented out chatbot until ready for deployment, edited footer.js

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <NavbarComponent />
         {children}
-        <Chatbot />
+        {/* <Chatbot /> */}
         <Footer />
       </body>
 
